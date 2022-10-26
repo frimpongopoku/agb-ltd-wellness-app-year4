3e718fbb147cfb8016b9cca90b3d626a62f28c55
frimpongOpoku <pongofrimi@gmail.com>
2022-10-26 22:09:00 +0400

    inital setup, port and express :ok_hand:

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+// ----------------------- CREATE LISTENING PORT HERE TO ENTER THE APP -
+
+app.listen(4000, () => {
+  console.log("listening to port 4000");
+  console.log("----------------------");
+});
