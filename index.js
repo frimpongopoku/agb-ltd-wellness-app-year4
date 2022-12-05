4d1e5f6cf4c6d3868392796b94e3196ba9f34c62
frimpongOpoku <pongofrimi@gmail.com>
2022-12-05 22:26:18 +0400

    some cors fixes

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ app.use(
     credentials: true,
   })
 );
+app.use(function(req, res, next) {  
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});  
+
 
 // ---------------- DECLARING MIDDLEWARE -----------------
 
